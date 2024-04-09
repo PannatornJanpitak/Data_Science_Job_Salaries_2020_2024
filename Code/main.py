a98fb291fadb44015ef1ee556ad9a5977ab4b8a9
anbun_ <166464529+PannatornJanpitak@users.noreply.github.com>
2024-04-09 09:00:27 +0700

    Add main file
    to run prediction app

---
+++ Code/main.py
@@ -0,0 +1,14 @@
+"""
+
+This file show training progress before getting the model.
+
+"""
+from UI_predict_salaries import SalaryPredictorApp
+
+def main():
+   app = SalaryPredictorApp()
+
+if __name__ == "__main__":
+   main()
+
+
